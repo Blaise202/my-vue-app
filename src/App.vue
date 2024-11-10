bf7fce27b52d92d670d81d499f35ea9a6a1c6452
Blaise202 <izerimanab74@gmail.com>
2024-11-10 09:15:14 +0200

    Applying CSS to the components

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <navbar
+    :pages="pages"
+    :active-page="activePage"
+    :nav-link-click="(index) => (activePage = index)"
+  ></navbar>
+  <page-viewer :page="pages[activePage]"></page-viewer>
+</template>
+
+<script>
+import PageViewer from "./components/PageViewer.vue";
+import Navbar from "./components/Navbar.vue";
+
+export default {
+  components: {
+    PageViewer,
+    Navbar,
+  },
+  data() {
+    return {
+      activePage: 0,
+      pages: [
+        {
+          link: { text: "Home", url: "index.html" },
+          pageTitle: "home page",
+          content: "this is home content",
+        },
+        {
+          link: { text: "Contact", url: "contact.html" },
+          pageTitle: "Contact page",
+          content: "this is Contact content",
+        },
+        {
+          link: { text: "About", url: "about.html" },
+          pageTitle: "About page",
+          content: "this is About content",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/NavbarLink.vue
@@ -0,0 +1,13 @@
+<template>
+  <a
+    :class="{ active: activePage == index }"
+    aria-current="page"
+    :href="page.link.url"
+    :title="`This link goes to ${page.link.text} page`"
+    @click.prevent="navLinkClick(index)"
+    class="nav-link"
+    >{{ page.link.text }}</a
+  >
+</template>
+
+<script></script>
